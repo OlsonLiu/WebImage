dd8b330da579e903728bab732a8d8b0fdc41a183
OLSON.LIU <olson.liu@deltaww.com>
2018-09-17 17:18:03 +0800

    prepare to write to local

--- src/main/kotlin/WebSite.kt
+++ src/main/kotlin/WebSite.kt
@@ -4,7 +4,7 @@ import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
 import org.springframework.util.StringUtils
 import java.io.File
-import java.nio.file.Files
+import java.nio.ByteBuffer
 
 /* @author  OLSON.LIU
  * @since   2018/9/17
@@ -17,29 +17,37 @@ class WebSite {
   }
 
   constructor()
-  constructor(localPath:String){
+  constructor(localPath: String) {
     storeImagePath = if (!existDirectory(localPath)) localPath else storeImagePath
   }
 
-
-
   fun grabWholeHtml(url: String) {
-    if (valiadteUrl(url)) {
+    if (validateUrl(url)) {
       val html: Document = Jsoup.connect(url).get()
       val imgElements: Elements = html.getElementsByTag("img")
       for (eachEle in imgElements) {
-        val img = eachEle.absUrl("src")
-        println("the image src : $img")
+        val imgUrl = eachEle.absUrl("src")
+        println("the image src : $imgUrl")
       }
     }
   }
 
-  private fun valiadteUrl(url: String): Boolean {
+  private fun download(imgUrl:String){
+    val imgBytes  = Jsoup.connect(imgUrl).ignoreContentType(true).execute().bodyAsBytes()
+    var buffer = ByteBuffer.wrap(imgBytes)
+  }
+
+  private fun moveToLocal(buffer: ByteBuffer){
+    var imgCnt = 1
+
+  }
+
+  private fun validateUrl(url: String): Boolean {
     val validator = UrlValidator(urlScheme)
     return validator.isValid(url)
   }
 
-  private fun existDirectory(localPath:String):Boolean{
+  private fun existDirectory(localPath: String): Boolean {
     return !StringUtils.isEmpty(localPath) && File(localPath).isDirectory
   }
 }
