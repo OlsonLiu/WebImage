3e4704705598766c75bd673c5db040886ca2e883
OlsonLiu <satis.iim03g@g2.nctu.edu.tw>
2018-09-23 13:49:38 +0800

    1. can download whole page 2. need more nullable cover

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -3,5 +3,5 @@
  * @version 1.0
  */
 fun main(args:Array<String>){
-  WebSite("D:\\").grabWholeHtml("https://stackoverflow.com/questions/4875064/jsoup-how-to-get-an-images-absolute-url")
+  WebSite("D:\\temp\\").grabWholeHtml("https://wow-kow.com/2017/12/06/%E8%94%A1%E8%AD%AF%E5%BF%83-%E9%9B%B2%E7%A7%91%E5%A4%A7%E6%A0%A1%E8%8A%B1candice%E9%80%86%E5%A4%A9%E7%BE%8E%E8%85%BF%E7%B0%A1%E7%9B%B4%E6%A5%B5%E5%93%81%E3%80%81%E8%B6%85%E6%AD%A3ol%E7%BE%8E/")
 }
--- src/main/kotlin/WebSite.kt
+++ src/main/kotlin/WebSite.kt
@@ -3,51 +3,66 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
 import org.springframework.util.StringUtils
+import java.io.ByteArrayInputStream
 import java.io.File
 import java.nio.ByteBuffer
+import javax.imageio.ImageIO
 
 /* @author  OLSON.LIU
  * @since   2018/9/17
  * @version 1.0
  */
 class WebSite {
-  companion object {
-    var storeImagePath = "C:\\"
-    val urlScheme = arrayOf("https", "http")
-  }
-
-  constructor()
-  constructor(localPath: String) {
-    storeImagePath = if (!existDirectory(localPath)) localPath else storeImagePath
-  }
-
-  fun grabWholeHtml(url: String) {
-    if (validateUrl(url)) {
-      val html: Document = Jsoup.connect(url).get()
-      val imgElements: Elements = html.getElementsByTag("img")
-      for (eachEle in imgElements) {
-        val imgUrl = eachEle.absUrl("src")
-        println("the image src : $imgUrl")
-      }
+    companion object {
+        var storeImagePath = "D:\\temp"
+        val urlScheme = arrayOf("https", "http")
     }
-  }
 
-  private fun download(imgUrl:String){
-    val imgBytes  = Jsoup.connect(imgUrl).ignoreContentType(true).execute().bodyAsBytes()
-    var buffer = ByteBuffer.wrap(imgBytes)
-  }
+    constructor()
+    constructor(localPath: String) {
+        //TODO find why this location cannt be replaced
+        storeImagePath = if (!existDirectory(localPath)) localPath else storeImagePath
+    }
 
-  private fun moveToLocal(buffer: ByteBuffer){
-    var imgCnt = 1
+    fun grabWholeHtml(url: String) {
+        if (validateUrl(url)) {
+            val html: Document = Jsoup.connect(url).get()
+            val imgElements: Elements = html.getElementsByTag("img")
+            for ((index, eachEle) in imgElements.withIndex()) {
 
-  }
+                val imgUrl = eachEle.absUrl("src")
+                download(imgUrl, index.toString() + ".jpg")
+            }
+        }
+    }
 
-  private fun validateUrl(url: String): Boolean {
-    val validator = UrlValidator(urlScheme)
-    return validator.isValid(url)
-  }
+    private fun download(imgUrl: String, fileName: String) {
+        var imgBytes: ByteArray? = ByteArray(0)
+        try {
+            imgBytes = Jsoup.connect(imgUrl)?.ignoreContentType(true)?.execute()?.bodyAsBytes()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            //TODO why this always process empty string
+            println("$imgUrl : connect fail")
+        }
+        var buffer = ByteBuffer.wrap(imgBytes)
+        moveToLocal(buffer, "$storeImagePath", fileName)
+    }
 
-  private fun existDirectory(localPath: String): Boolean {
-    return !StringUtils.isEmpty(localPath) && File(localPath).isDirectory
-  }
+    private fun moveToLocal(buffer: ByteBuffer, savePath: String, fileName: String) {
+        if (buffer.array().size < 15000) return // skip little icon
+        val filePath = "$savePath\\$fileName"
+        val img = ImageIO.read(ByteArrayInputStream(buffer.array()))
+        //TODO only do the jpg now, need more extensibility
+        ImageIO.write(img, "jpg", File(filePath))
+    }
+
+    private fun validateUrl(url: String): Boolean {
+        val validator = UrlValidator(urlScheme)
+        return validator.isValid(url)
+    }
+
+    private fun existDirectory(localPath: String): Boolean {
+        return !StringUtils.isEmpty(localPath) && File(localPath).isDirectory
+    }
 }
