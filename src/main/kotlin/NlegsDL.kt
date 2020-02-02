eaef438986f7be414dac853708217b58992b91d0
OlsonLiu <satis.iim03g@g2.nctu.edu.tw>
2020-02-02 11:35:05 +0800

    extract image parameter from href then build.gradle url to download

--- src/main/kotlin/NlegsDL.kt
+++ src/main/kotlin/NlegsDL.kt
@@ -1,12 +1,15 @@
+
 import org.openqa.selenium.By
 import org.openqa.selenium.chrome.ChromeDriver
 import java.awt.image.BufferedImage
 import java.io.File
 import java.net.HttpURLConnection
 import java.net.URL
+import java.net.URLConnection
 import java.util.stream.StreamSupport
 import javax.imageio.ImageIO
 import kotlin.streams.toList
+
 
 class NlegsDL {
   companion object {
@@ -38,22 +41,30 @@ class NlegsDL {
     val file = File("$src$directory\\")
     if (!file.isDirectory) file.mkdir()
 
-
     var elements = driver.findElements(By.cssSelector(".col-md-12.col-lg-12.panel.panel-default .panel-body a"))
     for ((index, webElement) in elements.withIndex()) {
       var fileNameIndex = index + 1
       if (File(file, "$fileNameIndex.jpg").exists()) continue;
 
       val urlStr = webElement.getAttribute("href").toString().trim()
-      var url = URL(urlStr)
+      val img = extractNeededParameter(urlStr)
+      val imgUrl = "http://nlegs.com/images/${img}.jpg"
+      var url = URL(imgUrl)
 
-      var connection: HttpURLConnection = url.openConnection() as HttpURLConnection  // kotlin cast
-      connection.addRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)")
+      var connection: URLConnection = url.openConnection() as HttpURLConnection  // kotlin cast
+      connection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36")
       connection.addRequestProperty("Referer", urlStr)
       val savedImg: BufferedImage = ImageIO.read(connection.inputStream)
 
       ImageIO.write(savedImg, "jpg", File(file, "${fileNameIndex}.jpg"))
     }
+  }
+
+  private fun extractNeededParameter(hrefString: String): String {
+    if (hrefString == null) throw IllegalArgumentException("error format href")
+    var scratch: List<String> = hrefString.split("&")
+    var imageStr = scratch.filter { (!it.isNullOrEmpty() && it.startsWith("img=")) }[0]
+    return imageStr.substringAfter("img=")
   }
 
   private fun filterGoodPart(siteName: String): Boolean {
